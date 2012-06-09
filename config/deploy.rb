e960d22d7cf7ac3d7dd73101592ab2a728160ff8
Alexey Galkin <aagalkin@gmail.com>
2012-06-09 23:45:54 +0400

    configuration for config.yml and bugfix in unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,12 +30,14 @@ namespace :deploy do
     sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
     run "mkdir -p #{shared_path}/config"
     put File.read("config/database.yml.example"), "#{shared_path}/config/database.yml"
+    put File.read("config/configuration.yml.example"), "#{shared_path}/config/configuration.yml"
     puts "Now edit the config files in #{shared_path}."
   end
   after "deploy:setup", "deploy:setup_config"
 
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/configuration.yml #{release_path}/config/configuration.yml"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
 
